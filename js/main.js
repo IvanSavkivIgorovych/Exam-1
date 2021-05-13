4d66b3fb10e67dd7da920402e854994cd93ad9d3
Savkiv <ivan.savkiv.igorovych>
2021-05-13 15:24:45 +0300

    Fix navigation

--- js/main.js
+++ js/main.js
@@ -14,13 +14,13 @@ $(document).ready(function () {
   $("#nav-icon2").click(function () {
     $(this).toggleClass("open");
     $(".header__menu").toggleClass("active");
-    if ($("#nav-icon2").hasClass("open")) {
-      $("#why").css("margin-top", 310);
-    } else {
-      var nav = $("#myNav").height();
-      var navPadding = $("#myNav").css("padding-top");
-      var a = parseInt(parseInt(nav) + parseInt(navPadding) * 2);
-      $("#why").css("margin-top", 80);
-    }
+    // if ($("#nav-icon2").hasClass("open")) {
+    //   $("#why").css("margin-top", 310);
+    // } else {
+    //   var nav = $("#myNav").height();
+    //   var navPadding = $("#myNav").css("padding-top");
+    //   var a = parseInt(parseInt(nav) + parseInt(navPadding) * 2);
+    //   $("#why").css("margin-top", 80);
+    // }
   });
 });
